dd659361fec809848677a8394e19e3af03336bb4
Clod <claudio.grasso@yahoo.com>
2025-06-23 20:50:40 -0300

    Compilation issues fixed

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:terra_cam/main.dart';
+import 'package:terracam/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
